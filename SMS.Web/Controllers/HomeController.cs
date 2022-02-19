c9bc9f54097779e928484240fda542d958131ace
Duncan Kane <duncan.kane1997@hotmail.com>
2022-02-19 10:56:33 +0000

    updated https settings for replit

--- SMS.Web/Controllers/HomeController.cs
+++ SMS.Web/Controllers/HomeController.cs
@@ -6,26 +6,9 @@ namespace SMS.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+     public IActionResult About()
     {
-        _logger = logger;
+        return Content("the about page");
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
